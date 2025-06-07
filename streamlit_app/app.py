b3f5ca55396b3e9a05965b77c8213445a5d38bd6
se000202 <77709607+se000202@users.noreply.github.com>
2025-06-07 14:03:56 +0900

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -8,7 +8,6 @@ load_dotenv()
 
 # API URL 설정 (fallback 포함)
 API_URL = os.getenv("FASTAPI_URL")
-st.write(f"API_URL = {API_URL}")
 if not API_URL:
     st.error("❌ API_URL is not set! Please check your environment variables.")
     st.stop()
